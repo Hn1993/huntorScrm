2e2c1066358adf9e666ac00b159c37860d139f3e
zixuan06010 <zixuan06010@sina.com>
2015-07-30 17:13:37 +0800

    修改点击已购用户出现的BUG

--- example/src/main/java/com/huntor/mscrm/app2/adapter/BuyedUserXdkAdapter.java
+++ example/src/main/java/com/huntor/mscrm/app2/adapter/BuyedUserXdkAdapter.java
@@ -2,6 +2,7 @@ package com.huntor.mscrm.app2.adapter;
 
 import android.app.Service;
 import android.content.Context;
+import android.nfc.Tag;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import com.huntor.mscrm.app2.R;
 import com.huntor.mscrm.app2.model.Fans;
 import com.huntor.mscrm.app2.utils.DateFormatUtils;
+import com.huntor.mscrm.app2.utils.MyApplication;
+import com.huntor.mscrm.app2.utils.MyLogger;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -17,8 +20,9 @@ import com.nostra13.universalimageloader.core.ImageLoader;
  * Created by Admin on 2015/5/18.
  */
 public class BuyedUserXdkAdapter extends MyBaseAdapter {
-
+    public String Tag = "BuyedUserXdkAdapter";
     private Context context;
+
     /**
      * 构造方法
      *
@@ -26,7 +30,7 @@ public class BuyedUserXdkAdapter extends MyBaseAdapter {
      */
     public BuyedUserXdkAdapter(Context c) {
         super(c);
-        this.context=c;
+        this.context = c;
     }
 
     /**
@@ -41,30 +45,31 @@ public class BuyedUserXdkAdapter extends MyBaseAdapter {
     public View getMyView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
         ViewHolder vh = null;
-        if(convertView==null){
-            vh=new ViewHolder();
-            convertView=inflater.inflate(R.layout.member_adapter_item,null);
+        if (convertView == null) {
+            vh = new ViewHolder();
+            convertView = inflater.inflate(R.layout.member_adapter_item, null);
             //vh.headPortrait = (ImageView) convertView.findViewById(R.id.head_portrait);
             vh.name = (TextView) convertView.findViewById(R.id.member_name);
-            vh.time = (TextView) convertView.findViewById(R.id.time);
+            vh.time = (TextView) convertView.findViewById(R.id.member_time);
             //vh.grade= (TextView) convertView.findViewById(R.id.tv_grade);
             convertView.setTag(vh);
 
-        }else{
+        } else {
             vh = (ViewHolder) convertView.getTag();
         }
         Fans fans = (Fans) list.get(position);
-        if(fans.name!=null&&!("".equals(fans.name))){
+        if (fans.name != null && !("".equals(fans.name))) {
             vh.name.setText(fans.name);
-        }else{
+        } else {
             vh.name.setText("暂无");
         }
         //registTime 注册时间
         //subscribeTime 关注时间
-        if(fans.subscribeTime!=0){
+        if (fans.subscribeTime != 0) {
             String time = DateFormatUtils.getPassedTime(context, fans.subscribeTime);
+            MyLogger.e(Tag, "time=" + time);
             vh.time.setText(time);
-        }else{
+        } else {
             vh.time.setText("");
         }
 //        ImageLoader imageLoader = ImageLoader.getInstance();
@@ -86,7 +91,7 @@ public class BuyedUserXdkAdapter extends MyBaseAdapter {
         return convertView;
     }
 
-    public class ViewHolder{
+    public class ViewHolder {
         TextView name;//姓名
         //ImageView headPortrait;//ͷ头像
         TextView time;//时间
